033fcefa1d2fe3411be513a01018cc4ded76d96d
John Luke Schaefer <lukeschaefer@gmail.com>
2018-02-13 19:18:35 -0800

    added 40 valentines

---
+++ main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    var valentinelist = {
+        0: "This relationship is my life's best work, and will likely always be it.",
+        1: "My love for you is deeper than the marianas trench, and they'll both crush a can in a split second!",
+        2: "There are few words I know to describe the way I feel.  They should have sent a poet.",
+        3: 'Every day that passes, I find that I love you more and more.',
+        4: 'I would go through all nine circles of hell and back for you.  Treachery would be difficult, but doable.',
+        5: "It's hard to get you out of my mind; just like that damned Havana song.",
+        6: "You've caught my eye like a Chinese Finger Trap catches my fingers, Valentine!",
+        7: "I am every bit yours, just as you are every bit mine.",
+        8: "You make every bad day worth every moment.",
+        9: "Ed Sheeran lyrics go here",
+        10: "Who knew that I'd spent my whole life looking for you only to run into you over a plate of chili fries?",
+        11: "You get me through every day.  Your presence is immeasurable.",
+        12: "You cement my will like a stone.  You make me feel like I can do anything.",
+        13: "I never knew how complete I could feel until being with you.",
+        14: "You are, have been, and always will be beautiful.",
+        15: "I'm so happy when we can watch horrible things together.",
+        16: "I so often wish we could just freeze time together.",
+        17: "If loving you is wrong, I don't wanna be right.",
+        18: "It's hard to believe we became such a perfect pairing.",
+        19: "If you were a library rental I'd get the shit fined out of me for not returning you.",
+        20: "I will always be here for you; physically or otherwise.",
+        21: "I hope your day is as nice as your boobs, Valentine.",
+        22: "You are my center, my strength, my motivation.  Don't ever stop.",
+        23: "You're the best partner in crime a dope like me could ask for.",
+        24: "Roses aren't a good symbol of our love.  Roses wilt and don't last.  Our love is more like a GameBoy; lasts even through a bomb blast.",
+        25: "You're at work while I'm writing these.  Please come back soon.",
+        26: "You grew on me like E.Coli grew on Chipotle's veggies!",
+        27: "If you were The Thing you could assimilate me anytime.",
+        28: "I love you for who you are but those boobs and lips sure don't hurt.",
+        29: "I would do things that would put me in front of the Hague for you.",
+        30: "I wake up every morning and choose you.",
+        31: "Ever since being with you, nobody else has been worth even thinking about.",
+        32: "Don't ever give up on me; I'll never give up on you.",
+        33: "You are my happy place.",
+        34: "I'm so glad we're one of those couples that won't settle on a dumb ass baby name.",
+        35: "If I had a nickel for every time I think of you, I'd have a nickel, cause you never leave my mind.",
+        36: "Every day I wake up next to you becomes even more worth it than the last.",
+        37: "It's only been 834 days for us.  Let's bump that up by a few thousand.",
+        38: "You are my home.  Wherever we are, I'm content.",
+        39: "I'm not sure that I knew who I was before I met you.",
+        40: "For pulling me out of myself, I can never repay you.  I owe you my life, my love, and my continued faith in myself and in us.",
+
+
+
+    }
+
+
+
+
+})
